46b624d92567664fc655576c58924737fca166ee
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-12 16:07:18 +0500

    added the numerical and categorical columns

--- final_pipeline.py
+++ final_pipeline.py
@@ -10,5 +10,7 @@ addiction_df = pd.read_csv("F://Smartphone_Addiction//Data//train.csv")
 X = addiction_df.drop(columns=["addicted_label"])
 y = addiction_df["addicted_label"]
 
-
+numeric_coloumns_median = ["age" , "daily_screen_time_hours" , "sleep_hours" , "notifications_per_day" , "app_opens_per_day" , "weekend_screen_time" , "work_study_hours"]
+numeric_column_zero = ["social_media_hours" , "gaming_hours"]
+categorical_columns = ["gender", "stress_level", "academic_work_impact"]
 
