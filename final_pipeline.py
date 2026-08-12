7aa5ad7148197fdddf2c40a637dbffd504c82390
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-12 16:06:31 +0500

    added the dataset

--- final_pipeline.py
+++ final_pipeline.py
@@ -1,6 +1,14 @@
+import pandas as pd
 from sklearn.pipeline import Pipeline
 from xgboost import XGBClassifier
 from sklearn.preprocessing import OneHotEncoder , StandardScaler
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
+import joblib
+
+addiction_df = pd.read_csv("F://Smartphone_Addiction//Data//train.csv")
+X = addiction_df.drop(columns=["addicted_label"])
+y = addiction_df["addicted_label"]
+
+
 
