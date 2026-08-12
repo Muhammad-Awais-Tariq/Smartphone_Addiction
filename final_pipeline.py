afdebe99e474cbfe5bad983416b81a38287d1ed6
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-12 16:05:27 +0500

    added the final pipeline

---
+++ final_pipeline.py
@@ -0,0 +1,6 @@
+from sklearn.pipeline import Pipeline
+from xgboost import XGBClassifier
+from sklearn.preprocessing import OneHotEncoder , StandardScaler
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+
